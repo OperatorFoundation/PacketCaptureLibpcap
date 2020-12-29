b07a554cfbb70bc7f339d3a2ea3b8ca726100d18
consuelita <adelita@operatorfoundation.org>
2020-12-29 14:47:12 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/OperatorFoundation/PacketStream.git", from: "0.0.1"),
-        .package(url: "https://github.com/OperatorFoundation/SwiftPCAP.git", from: "1.1.9"),
+        .package(url: "https://github.com/OperatorFoundation/SwiftPCAP.git", from: "1.1.10"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
